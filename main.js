8443218bf97f8226aec4d1b302741f627c3c83ef
Angelo <angelo.vagenas1@bellmedia.ca>
2024-06-19 15:59:11 -0400

    adding more

---
+++ main.js
@@ -0,0 +1,12 @@
+(() => {
+    const scriptSrc = new URL(document.currentScript.src);
+    const scriptSrcPathName = scriptSrc.pathname;
+    const scriptTags = Array.from(document.getElementsByTagName("script")) ?? [];
+    const basePath = scriptTags
+        .map(tag => console.log(tag) || tag.src)
+        .map(tag => tag.src)
+        .filter(src => src.includes(scriptSrcPathName))
+        .reduce((loaderSrc, currentSrc) => currentSrc ? new URL(currentSrc).origin : undefined, undefined);
+
+    console.log(basePath);
+})();
